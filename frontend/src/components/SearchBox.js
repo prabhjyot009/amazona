d34b95939ddf1d4b19318312d129ba274b6bca52
prabhjyot009 <prabh156dps@gmail.com>
2023-03-24 00:49:52 +0530

    commit

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -1,19 +1,19 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import FormControl from 'react-bootstrap/FormControl';
+import InputGroup from 'react-bootstrap/InputGroup';//importing react-bootstrap components
+import FormControl from 'react-bootstrap/FormControl';//importing react-bootstrap components
 import { useNavigate } from 'react-router-dom';
 
-export default function SearchBox() {
-  const navigate = useNavigate();
-  const [query, setQuery] = useState('');
-  const submitHandler = (e) => {
+export default function SearchBox() {//SearchBox is a function that takes in no arguments
+  const navigate = useNavigate();//navigate is a variable that is equal to the useNavigate function
+  const [query, setQuery] = useState('');//query is a variable that is equal to the useState function
+  const submitHandler = (e) => {//submitHandler is a function that takes in an object as an argument
     e.preventDefault();
     navigate(query ? `/search/?query=${query}` : '/search');
   };
 
-  return (
+  return (//returning the following jsx code to the caller of this function which is equal to the SearchBox object's __proto__ property which is equal to the function's prototype property which is equal to an object that has a constructor property which is equal to the SearchBox function
     <Form className="d-flex me-auto" onSubmit={submitHandler}>
       <InputGroup>
         <FormControl
